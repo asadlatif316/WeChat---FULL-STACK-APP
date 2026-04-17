01173b56da1491761b70526dd9f5a3298c779b91
Asad <hellohello51981@gmail.com>
2026-04-17 11:43:47 +0500

    feat: hashPassword utils

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,16 +1,13 @@
-import StatusCodes from 'http-status-codes'
-import User from '../models/userModel.js'
+import StatusCodes from 'http-status-codes';
+import User from '../models/userModel.js';
+import { hashPassword } from '../utils/password.js';
 
 const register = async (req, res) => {
-    const { email, password, name } = req.body
-    console.log(req.body);
-    
-    if (!email || !password || !name) {
-        return res.status(StatusCodes.BAD_REQUEST).json({ error: 'All fields are required'})
-    }
-    
-    const user = await User.create(req.body)
-  res.status(StatusCodes.CREATED).json({user});
+  const hashedPassword = await hashPassword(req.body.password);
+  req.body.password = hashedPassword;
+
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
---
+++ server/src/utils/password.js
@@ -0,0 +1,9 @@
+import bcrypt from 'bcryptjs';
+
+export const hashPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  const hashPass = await bcrypt.hash(password, salt);
+  return hashPass;
+};
+
+
