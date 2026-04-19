055fd0499366e5e8709e0f385e1b9d9573eeefc5
Asad <hellohello51981@gmail.com>
2026-04-19 16:41:21 +0500

    feat: register Input validations added

---
+++ server/src/middlewares/validateMiddleware.js
@@ -0,0 +1,42 @@
+import { body, validationResult } from 'express-validator';
+import { StatusCodes } from 'http-status-codes';
+import User from '../models/userModel.js'
+const withValidationError = (validationValues) => {
+    return [
+        validationValues,
+        (req, res, next) => {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                const errorMessage = errors.array().map(error => error.msg)
+                res.status(StatusCodes.BAD_REQUEST).json({error: errorMessage})
+            }
+            next()
+        }
+    ]
+}
+
+
+export const validateRegisterInput = withValidationError([
+  body('name')
+    .notEmpty()
+    .withMessage('name is required')
+    .isLength({ min: 3, max: 20 })
+    .withMessage('name must be between 3 to 20 characters long')
+    .trim(),
+  body('email')
+    .notEmpty()
+    .withMessage(' email is required')
+    .isEmail()
+    .withMessage('invalid email format')
+    .custom(async (email) => {
+      const user = await User.findOne({ email });
+      if (user) {
+        throw new Error('email already exist')
+      }
+    }),
+  body('password')
+    .notEmpty()
+    .withMessage('password is required')
+    .isLength({ min: 8 })
+    .withMessage('password must be at least 8 characters long')
+]);
--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express'
 const router = express.Router()
 import { login, refreshToken, register } from '../controllers/authController.js'
+import { validateRegisterInput } from '../middlewares/validateMiddleware.js'
 
-
-router.route('/register').post(register)
+router.route('/register').post(validateRegisterInput, register);
 router.route('/login').post(login)
 router.route('/refresh-token').get(refreshToken)
 
