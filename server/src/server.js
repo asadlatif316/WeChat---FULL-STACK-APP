122a52206d13432356cd3b7ef323a6780733f8b2
Asad <hellohello51981@gmail.com>
2026-04-19 15:24:08 +0500

    feat: express validation setup

--- server/src/server.js
+++ server/src/server.js
@@ -2,52 +2,65 @@
 import dotenv from 'dotenv';
 dotenv.config({ quiet: true });
 import express from 'express';
-const app = express();
-import cookieParser from 'cookie-parser'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import cookieParser from 'cookie-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-const __dirname = dirname(fileURLToPath(import.meta.url))
+import { body, validationResult } from 'express-validator';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
 
 //other imports
-import authRouter from './routes/authRoutes.js'
+import authRouter from './routes/authRoutes.js';
 import connectDB from './DB/connect.js';
+import { log } from 'console';
+import { StatusCodes } from 'http-status-codes';
 //middlewares
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 //routes
-app.use('/api/v1/auth',authRouter)
-
-
-
+app.use('/api/v1/auth', authRouter);
 
 // //to ready for development
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, '../../client/dist')))
+  app.use(express.static(path.resolve(__dirname, '../../client/dist')));
   app.get('*splat', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'../client','dist','index.html'))
-  })
+    res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
+  });
 }
-
 
 app.get('/', (req, res) => {
   res.send('working on backend');
 });
-const port = process.env.PORT || 4200;
 
+app.post(
+  '/api/v1/test',
+  [body('name').notEmpty().withMessage('name is required')],
+  (req, res, next) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      const errorMessage = error.array().map((error) => error.msg);
+      return res.status(StatusCodes.BAD_REQUEST).json({ error: errorMessage });
+    }
+    next();
+  },
+  (req, res) => {
+    const { name } = req.body;
+    res.json({ msg: `hello ${name}` });
+  },
+);
+const port = process.env.PORT || 4200;
 
 const startApp = async () => {
   try {
-    await connectDB(process.env.MONGO_URL)
+    await connectDB(process.env.MONGO_URL);
     app.listen(port, () => {
       console.log(`server is running on port ${port}`);
     });
-
   } catch (error) {
-    process.exit(1)
+    process.exit(1);
   }
-}
+};
 
-startApp()
-
+startApp();
