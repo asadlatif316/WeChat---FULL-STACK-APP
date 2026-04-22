b967e1f136346d977b0630d3f072141adb705a5b
Asad <hellohello51981@gmail.com>
2026-04-22 06:00:12 +0500

    refactor: change route name

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -5,7 +5,7 @@ import protectUser from '../middlewares/authMiddleware.js';
 
 
 
-router.route('/conversations').get(protectUser, getAllUsers);
+router.route('/contacts').get(protectUser, getAllUsers);
 router.route('/get-user').get(protectUser,getUser);
 router.route('/update-user').get(updateUser);
 
