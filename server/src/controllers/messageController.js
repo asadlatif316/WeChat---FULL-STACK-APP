85281a4019421161c673e1e49553459c829090d5
Asad <hellohello51981@gmail.com>
2026-04-25 10:29:03 +0500

    feat: send Message function added

--- server/src/controllers/messageController.js
+++ server/src/controllers/messageController.js
@@ -1,5 +1,35 @@
+import cloudinary from '../lib/cloudinary.js';
+import Messages from '../models/messageModel.js';
+import Conversation from '../models/conversationModel.js';
+import { StatusCodes } from 'http-status-codes';
+
 const sendMessage = async (req, res) => {
-  res.json('send message');
+  const { content, image } = req.body;
+  const { conversationId } = req.params;
+  const sender = req.user.userId;
+
+  let ImageUrl;
+  if (image) {
+    const uploadResponse = await cloudinary.uploader.upload(image);
+    ImageUrl = uploadResponse.secure_url;
+  }
+
+  const newMessage = new Messages({
+    conversationId,
+    sender,
+    content,
+    image: ImageUrl,
+  });
+
+  await newMessage.save();
+
+  await Conversation.findByIdAndUpdate(conversationId, {
+    latestMessage: newMessage._id,
+  });
+
+  await newMessage.populate('sender', 'name email avatar');
+
+  res.status(StatusCodes.OK).json(newMessage);
 };
 
 const getMessageById = async (req, res) => {
