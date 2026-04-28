d0e354158a053d9fa209c6fb6a242077d586838f
Asad <hellohello51981@gmail.com>
2026-04-29 01:17:29 +0500

    feat: add Form Row Component

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,8 @@
 import { useAuthStore } from '@/store/useAuthStore';
-import { Input, Button } from '@/components/ui';
+import { Input, Button, FormRow } from '@/components/ui';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import logo from '../assets/logo.png'
+import logo from '../assets/logo.png';
 
 const Register = () => {
   const { signup, isSigningUp } = useAuthStore();
@@ -14,10 +14,9 @@ const Register = () => {
   });
 
   const handleChange = (e) => {
-    const {name,value} = e.target
+    const { name, value } = e.target;
     setFormData((prev) => ({ ...prev, [name]: value }));
     console.log(formData);
-    
   };
 
   const handleSubmit = async (e) => {
@@ -26,15 +25,14 @@ const Register = () => {
     navigate('/login');
   };
 
-
   return (
     <section className='p-5 authBg h-screen flex items-center justify-center text-foreground'>
-      <div className='w-full max-w-6xl md:h-[800x] h-[550px] bg-background rounded-2xl shadow-2xs'>
+      <div className='w-full max-w-6xl h-[550px] md:h-[650px] bg-background rounded-2xl shadow-2xs'>
         {/* left side */}
         <div className=' p-8 w-1/2 h-full flex flex-col items-center justify-center md:border-r border-b-emerald-400-600/30'>
           <div className='w-full max-w-md'>
             <div className=' flex flex-col items-center'>
-              <img src={logo} alt="" className='h-10 mb-4'/>
+              <img src={logo} alt='' className='h-10 mb-4' />
               <h2 className='text-2xl font-bold mb-2'>Create Account</h2>
               <p className='text-muted-foreground text-sm'>
                 Sign up for new Account
@@ -45,47 +43,29 @@ const Register = () => {
               onSubmit={handleSubmit}
               className='p-5 flex flex-col gap-y-4'
             >
-              <div className='flex flex-col gap-y-2'>
-                <label htmlFor='name' className=''>
-                  Name
-                </label>
-                <Input
-                  id='name'
-                  name='name'
-                  placeholder='Enter your name'
-                  className='p-5'
-                  value={formData.name}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className='flex flex-col gap-y-2 text-sm '>
-                <label htmlFor='email' className='text-sm'>
-                  Email
-                </label>
-                <Input
-                  id='email'
-                  name='email'
-                  value={formData.email}
-                  type='email'
-                  placeholder='Enter your email'
-                  className='p-5'
-                  onChange={handleChange}
-                />
-              </div>
-              <div className='flex flex-col gap-y-2'>
-                <label htmlFor='password' className='text-sm'>
-                  Password
-                </label>
-                <Input
-                  id='password'
-                  name='password'
-                  value={formData.password}
-                  type='password'
-                  placeholder='Enter your name'
-                  className='p-5'
-                  onChange={handleChange}
-                />
-              </div>
+              <FormRow
+                label='Name'
+                placeholder='Enter your name'
+                name='name'
+                value={formData.name}
+                onChange={handleChange}
+              />
+              <FormRow
+                label='Email'
+                placeholder='Enter your email'
+                name='email'
+                type='email'
+                value={formData.email}
+                onChange={handleChange}
+              />
+              <FormRow
+                label='Password'
+                placeholder='Password'
+                name='password'
+                type='password'
+                value={formData.password}
+                onChange={handleChange}
+              />
               <Button
                 disabled={isSigningUp}
                 size='lg'
@@ -96,8 +76,7 @@ const Register = () => {
             </form>
             <div className='text-center'>
               <Button className='text-center' variant='secondary'>
-                <Link to='/login'>
-                already have an account? Login</Link>
+                <Link to='/login'>already have an account? Login</Link>
               </Button>
             </div>
           </div>
