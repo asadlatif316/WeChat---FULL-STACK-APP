7c05cb8b463485ff363cf111a24de20dfdb4e097
Asad <hellohello51981@gmail.com>
2026-04-29 01:01:17 +0500

    feat: complete form styling

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,9 @@
 import { useAuthStore } from '@/store/useAuthStore';
-import { Form } from 'react-router-dom';
 import { Input, Button } from '@/components/ui';
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import logo from '../assets/logo.png'
+
 const Register = () => {
   const { signup, isSigningUp } = useAuthStore();
   const navigate = useNavigate();
@@ -27,58 +28,80 @@ const Register = () => {
 
 
   return (
-    <section className='p-5 authBg h-screen flex items-center'>
-      <div className='w-full max-w-6xl md:h-[600x] h-[650px] bg-secondary rounded-2xl shadow-2xs'>
-        <form
-          method='post'
-          onSubmit={handleSubmit}
-          className='p-5 flex flex-col gap-y-4'
-        >
-          <div className='flex flex-col gap-y-2'>
-            <label htmlFor='name' className=''>
-              Name
-            </label>
-            <Input
-              id='name'
-              name='name'
-              placeholder='Enter your name'
-              className='p-5'
-              value={formData.name}
-              onChange={handleChange}
-            />
+    <section className='p-5 authBg h-screen flex items-center justify-center text-foreground'>
+      <div className='w-full max-w-6xl md:h-[800x] h-[550px] bg-background rounded-2xl shadow-2xs'>
+        {/* left side */}
+        <div className=' p-8 w-1/2 h-full flex flex-col items-center justify-center md:border-r border-b-emerald-400-600/30'>
+          <div className='w-full max-w-md'>
+            <div className=' flex flex-col items-center'>
+              <img src={logo} alt="" className='h-10 mb-4'/>
+              <h2 className='text-2xl font-bold mb-2'>Create Account</h2>
+              <p className='text-muted-foreground text-sm'>
+                Sign up for new Account
+              </p>
+            </div>
+            <form
+              method='post'
+              onSubmit={handleSubmit}
+              className='p-5 flex flex-col gap-y-4'
+            >
+              <div className='flex flex-col gap-y-2'>
+                <label htmlFor='name' className=''>
+                  Name
+                </label>
+                <Input
+                  id='name'
+                  name='name'
+                  placeholder='Enter your name'
+                  className='p-5'
+                  value={formData.name}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className='flex flex-col gap-y-2 text-sm '>
+                <label htmlFor='email' className='text-sm'>
+                  Email
+                </label>
+                <Input
+                  id='email'
+                  name='email'
+                  value={formData.email}
+                  type='email'
+                  placeholder='Enter your email'
+                  className='p-5'
+                  onChange={handleChange}
+                />
+              </div>
+              <div className='flex flex-col gap-y-2'>
+                <label htmlFor='password' className='text-sm'>
+                  Password
+                </label>
+                <Input
+                  id='password'
+                  name='password'
+                  value={formData.password}
+                  type='password'
+                  placeholder='Enter your name'
+                  className='p-5'
+                  onChange={handleChange}
+                />
+              </div>
+              <Button
+                disabled={isSigningUp}
+                size='lg'
+                className='p-5 font-bold'
+              >
+                Register
+              </Button>
+            </form>
+            <div className='text-center'>
+              <Button className='text-center' variant='secondary'>
+                <Link to='/login'>
+                already have an account? Login</Link>
+              </Button>
+            </div>
           </div>
-          <div className='flex flex-col gap-y-2'>
-            <label htmlFor='email' className=''>
-              Email
-            </label>
-            <Input
-              id='email'
-              name='email'
-              value={formData.email}
-              type='email'
-              placeholder='Enter your email'
-              className='p-5'
-              onChange={handleChange}
-            />
-          </div>
-          <div className='flex flex-col gap-y-2'>
-            <label htmlFor='password' className=''>
-              Password
-            </label>
-            <Input
-              id='password'
-              name='password'
-              value={formData.password}
-              type='password'
-              placeholder='Enter your name'
-              className='p-5'
-              onChange={handleChange}
-            />
-          </div>
-          <Button disabled={isSigningUp} size='lg' className='p-5 font-bold'>
-            Register
-          </Button>
-        </form>
+        </div>
       </div>
     </section>
   );
