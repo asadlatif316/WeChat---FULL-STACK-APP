1fbdb8c56d12a40658b7c922cea658d0d2031816
Asad <hellohello51981@gmail.com>
2026-04-30 00:09:00 +0500

    feat: complete register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,10 @@
 import { useAuthStore } from '@/store/useAuthStore';
-import { Input, Button, FormRow, Spinner } from '@/components/ui';
+import { Button, Spinner } from '@/components/ui';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import logo from '../assets/logo.png';
+import signupImg from '../assets/signup.png';
+import { FormRow } from '@/components/ui';
 
 const Register = () => {
   const { signup, isSigningUp } = useAuthStore();
@@ -28,82 +30,84 @@ const Register = () => {
   return (
     <section className='p-5 authBg h-screen flex items-center justify-center text-foreground'>
       <div className='w-full max-w-6xl h-[550px] md:h-[650px] bg-background rounded-2xl shadow-2xs'>
-        {/* left side */}
-        <div className=' p-8 w-1/2 h-full flex flex-col items-center justify-center md:border-r border-b-emerald-400-600/30'>
-          <div className='w-full max-w-md'>
-            <div className=' flex flex-col items-center'>
-              <img src={logo} alt='' className='h-10 mb-4' />
-              <h2 className='text-2xl font-bold mb-2'>Create Account</h2>
-              <p className='text-muted-foreground text-sm'>
-                Sign up for new Account
-              </p>
-            </div>
-            <form
-              method='post'
-              onSubmit={handleSubmit}
-              className='p-5 flex flex-col gap-y-4'
-            >
-              <FormRow
-                label='Name'
-                placeholder='Enter your name'
-                name='name'
-                value={formData.name}
-                onChange={handleChange}
-              />
-              <FormRow
-                label='Email'
-                placeholder='Enter your email'
-                name='email'
-                type='email'
-                value={formData.email}
-                onChange={handleChange}
-              />
-              <FormRow
-                label='Password'
-                placeholder='Password'
-                name='password'
-                type='password'
-                value={formData.password}
-                onChange={handleChange}
-              />
-              <Button
-                disabled={isSigningUp}
-                size='lg'
-                className='p-5 font-bold'
+        <div className='w-full h-full flex flex-col md:flex-row'>
+          {/* left side */}
+          <div className=' p-8 w-1/2 h-full flex flex-col items-center justify-center md:border-r border-b-emerald-400-600/30'>
+            <div className='w-full max-w-md'>
+              <div className=' flex flex-col items-center'>
+                <img src={logo} alt='' className='h-10 mb-4' />
+                <h2 className='text-2xl font-bold mb-2'>Create Account</h2>
+                <p className='text-muted-foreground text-sm'>
+                  Sign up for new Account
+                </p>
+              </div>
+              <form
+                method='post'
+                onSubmit={handleSubmit}
+                className='p-5 flex flex-col gap-y-4'
               >
-                {isSigningUp ? (
-                  <>
-                    <Spinner /> Signing Up...
-                  </>
-                ) : (
-                  'Register'
-                )}
-              </Button>
-            </form>
-            <div className='text-center'>
-              <Button className='text-center' variant='secondary'>
-                <Link to='/login'>already have an account? Login</Link>
-              </Button>
+                <FormRow
+                  label='Name'
+                  placeholder='Enter your name'
+                  name='name'
+                  value={formData.name}
+                  onChange={handleChange}
+                />
+                <FormRow
+                  label='Email'
+                  placeholder='Enter your email'
+                  name='email'
+                  type='email'
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+                <FormRow
+                  label='Password'
+                  placeholder='Password'
+                  name='password'
+                  type='password'
+                  value={formData.password}
+                  onChange={handleChange}
+                />
+                <Button
+                  disabled={isSigningUp}
+                  size='lg'
+                  className='p-5 font-bold'
+                >
+                  {isSigningUp ? (
+                    <>
+                      <Spinner /> Signing Up...
+                    </>
+                  ) : (
+                    'Register'
+                  )}
+                </Button>
+              </form>
+              <div className='text-center'>
+                <Button className='text-center' variant='secondary'>
+                  <Link to='/login'>already have an account? Login</Link>
+                </Button>
+              </div>
             </div>
           </div>
-        </div>
-        {/* Right Side */}
-        <div className='hidden md:w-1/2 md:flex items-center justify-center p-6'>
-          <div>
-            <img
-              src={loginImg}
-              className='w-full h-auto object-contain'
-              alt=''
-            />
-            <div className='mt-6 text-center'>
-              <h3 className='text-xl text-muted-background font-semibold'>
-                Start Your Journey Today
-              </h3>
-            </div>
-            <div className='mt-4 flex justify-center gap-4'>
-              <span>Free</span>
-              <span>Easy setup</span>
-              <span>Private</span>
+          {/* Right Side */}
+          <div className='hidden md:w-1/2 md:flex items-center justify-center p-6'>
+            <div>
+              <img
+                src={signupImg}
+                className='w-full h-auto object-contain'
+                alt=''
+              />
+              <div className='mt-6 text-center'>
+                <h3 className='text-xl text-primary font-semibold'>
+                  Start Your Journey Today
+                </h3>
+              </div>
+              <div className='mt-4 text-primary flex justify-center gap-4'>
+                <span>Free</span>
+                <span>Easy setup</span>
+                <span>Private</span>
+              </div>
             </div>
           </div>
         </div>
