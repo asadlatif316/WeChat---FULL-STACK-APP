cc3b35f1de51dd046590e3a65e319907a69659a5
Asad <hellohello51981@gmail.com>
2026-08-20 11:10:08 +0500

    feat: send Image and preview image

--- client/src/components/MessageInput.jsx
+++ client/src/components/MessageInput.jsx
@@ -2,8 +2,9 @@ import { useChatStore } from '@/store/useChatStore';
 import { Button, Input } from './ui';
 import { IoIosAddCircle } from 'react-icons/io';
 import { IoSend } from 'react-icons/io5';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { useAuthStore } from '@/store/useAuthStore';
+import { IoMdClose } from 'react-icons/io';
 
 const MessageInput = () => {
   const { user } = useAuthStore();
@@ -12,25 +13,26 @@ const MessageInput = () => {
     setSelectedConversation,
     setActiveTab,
     selectedUser,
-    serSelectedUser,
     stopTyping,
   } = useChatStore();
   const [text, setText] = useState('');
-  const { sendMessage, showTyping, chats } = useChatStore();
+  const [imagePreview, setImagePreview] = useState(null);
+  const { sendMessage, showTyping } = useChatStore();
+  const fileInputRef = useRef(null);
 
   const person = selectedConversation
     ? selectedConversation.participants.find((p) => p._id !== user?._id)
     : selectedUser;
-  const handleSendMessage =async (e) => {
+  const handleSendMessage = async (e) => {
     e.preventDefault();
     if (!text.trim()) return;
     const conversation = await sendMessage({ content: text.trim() });
-  setText('');
+    setText('');
 
-  if (conversation) {
-    setSelectedConversation(conversation);
-    setActiveTab('chats');
-  }
+    if (conversation) {
+      setSelectedConversation(conversation);
+      setActiveTab('chats');
+    }
   };
 
   const handleChange = (e) => {
@@ -43,8 +45,38 @@ const MessageInput = () => {
     }, 2000);
   };
 
+  const handleImageChange = (e) => {const file = e.target.files[0];
+  if (!file) return;
+  if (file.size > 5 * 1024 * 1024) {
+    toast.error('Image must be under 2MB');
+    return;
+  }
+  const reader = new FileReader();
+  reader.readAsDataURL(file);
+
+  reader.onload = async () => {
+    const base64Image = reader.result;
+    setImagePreview(base64Image);
+  };};
+
   return (
     <div className=' mt-4'>
+      {imagePreview && (
+        <div className='max-w-3xl mx-auto mb-2 flex item-center'>
+          <div className='relative'>
+            <img
+              src={imagePreview}
+              className='w-20 h-20 object-cover rounded-md border'
+            />
+            <Button
+              className='absolute -top-1.5 -right-1.5 bg-gray-700 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs shadow'
+              onClick={() => setImagePreview(null)}
+            >
+              <IoMdClose />
+            </Button>
+          </div>
+        </div>
+      )}
       <form onSubmit={handleSendMessage} className='relative'>
         <Input
           className='pl-10 pr-18 py-7'
@@ -52,9 +84,19 @@ const MessageInput = () => {
           value={text}
           onChange={handleChange}
         />
-        <IoIosAddCircle className='absolute top-4.5 left-2 w-6 h-6 text-gray-500' />
+        <IoIosAddCircle
+          className='absolute top-4.5 left-2 w-6 h-6 text-gray-500 cursor-pointer'
+          onClick={() => fileInputRef.current.click()}
+        />
+        <Input
+          type='file'
+          accept='image/*'
+          className='hidden'
+          ref={fileInputRef}
+          onChange={handleImageChange}
+        />
         <Button
-          disabled={!text.trim()}
+          disabled={!text.trim() && !imagePreview}
           className='absolute top-2.5 right-4 flex justify-center items-center shadow-2xl h-10 w-10'
         >
           <IoSend className='w-6 h-6 text-white' />
